e5ddf3ad6b12853e0eb4ec9b5f23a413db005040
Damilola  Williams <36074540+dwilliams98@users.noreply.github.com>
2025-06-20 19:37:43 -0700

    Fix Dialog accessibility error

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -43,8 +43,6 @@ const DialogContent = React.forwardRef<
       )}
       {...props}
     >
-      <DialogTitle className="sr-only">Dialog</DialogTitle>
-      <DialogDescription className="sr-only">Dialog content</DialogDescription>
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
         <X className="h-4 w-4" />
